884612b48ce27dd2475b29b99e687d62fc0da89e
boom <boom@insta.com>
2023-05-21 07:12:02 +0000

    Add SIGINT handle

--- src/mian.c
+++ src/mian.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
 
 #include "memory.h"
 
+void INT_handler(int arg)
+{
+  int status;
+  Memory_Allocal_Manage_Deinit();
+  exit(status);
+  printf("%d\n",status);
+}
+
 int main() {
   Memory_Allocal_Manage_Init();
-  int* addr = (int*)Memory_Allocal_Get(TYPE_HUGE);
+  int* addr = (int*)Memory_Allocal_Get(TYPE_SMALL);
   Memory_Allocal_Release(&addr);
   Memory_Allocal_Get(TYPE_HUGE);
   Memory_Allocal_Get(TYPE_HUGE);
@@ -26,8 +36,7 @@ int main() {
   Memory_Allocal_Get(TYPE_HUGE);
 
 
-  Memory_Allocal_Manage_Deinit();
-
+  signal(SIGINT,INT_handler);
   for (;;) {
   }
 }
